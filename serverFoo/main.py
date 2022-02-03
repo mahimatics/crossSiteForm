36c58b9dbb8122c258f6cd037a9dca1d5bed2264
Mahidhar <mahidhar.chellamani@datakaveri.org>
2022-02-03 18:58:42 +0530

    Finish first draft of serverFoo

--- serverFoo/main.py
+++ serverFoo/main.py
@@ -1,8 +1,16 @@
-from flask import Flask
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 
-@app.route("/")
+@app.get("/")
 def hello_world():
-    return "Hello world"
+    secret = "?w5J;#2w6phZ^MJt'k:e{ $uUrnR%%:4OQh6eY4Sduuor|01{2UmNMr;yhKKQMk$"
+    submit_url = "/"
+    return render_template("hello.html", secret=secret, submit_url=submit_url)
+
+@app.post("/")
+def submit():
+    form = request.form
+    print(form)
+    return "Thanks"
